3c5eb5ce52a89883e0cd887398b7d8917477c4fa
ADDB <addb1234567890@gmail.com>
2023-10-30 00:31:10 +0100

    v1.0.8

--- content/Inkbound.PluginTemplate/Plugin.cs
+++ content/Inkbound.PluginTemplate/Plugin.cs
@@ -1,5 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
 
 namespace Inkbound.PluginTemplate {
 	[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
@@ -7,10 +9,12 @@ namespace Inkbound.PluginTemplate {
 		public const string PLUGIN_GUID = "Inkbound.PluginTemplate";
 		public const string PLUGIN_NAME = "Template Plugin";
 		public const string PLUGIN_VERSION = "1.0.0";
+        	public static readonly Harmony HarmonyInstance = new Harmony(PLUGIN_GUID);
         	internal static ManualLogSource log;
 		private void Awake() {
 			// Plugin startup logic
 			log = Logger;
+			HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
 			Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
     		}
 	}
